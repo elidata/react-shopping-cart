f1c4a233ece0a648f7e9622f117073c18dd37d47
elidata <eliedaat@gmail.com>
2022-06-13 13:53:02 +0300

    seperated products.js to another file

---
+++ src/products.js
@@ -0,0 +1,43 @@
+const PRODUCTS = [
+    {
+      name: 'apple',
+      pluralName: 'apples',
+      image: 'images/apple.png',
+      priceText: '$0.25 each',
+  
+      price: (quantity) => {
+        return quantity * 25;
+      }
+    },
+    {
+      name: 'banana',
+      pluralName: 'bananas',
+      image: 'images/banana.png',
+      priceText: '$0.15 each',
+      price: (quantity) => {
+        return quantity * 15;
+      }
+    },
+    {
+      name: 'orange',
+      pluralName: 'oranges',
+      image: 'images/orange.png',
+      priceText: '$0.30 each',
+      price: (quantity) => {
+        return quantity * 30;
+      }
+    },
+    {
+      name: 'papaya',
+      pluralName: 'papayas',
+      image: 'images/papaya.png',
+      priceText: '$0.50 each',
+      dealText: '3 for $1',
+      price: (quantity) => {
+        let odds = quantity % 3;
+        return (((quantity - odds)/3) * 100) + (odds * 50);
+      }
+    },
+  ];
+
+  export const products = PRODUCTS ;
